b815d22927805c7c9e013249ec4d9f9e639c8ca5
Joao Marcelo <joao.mm.santos@outlook.com>
2024-01-27 15:35:18 -0300

    service layer

---
+++ src/main/java/com/coursejava/courseWebService/services/UserService.java
@@ -0,0 +1,27 @@
+package com.coursejava.courseWebService.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coursejava.courseWebService.entities.User;
+import com.coursejava.courseWebService.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
