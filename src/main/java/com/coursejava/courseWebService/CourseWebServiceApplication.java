521ea13b894ecdfe3cdd57630f9951064a43777a
Joao Marcelo <joao.mm.santos@outlook.com>
2024-01-27 14:08:57 -0300

    project created

---
+++ src/main/java/com/coursejava/courseWebService/CourseWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coursejava.courseWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursejava/courseWebService/CourseWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursejava.courseWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
